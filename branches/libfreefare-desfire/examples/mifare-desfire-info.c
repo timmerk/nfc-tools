0fe59d1ce89bca868cc4985823b978faf11c995e
rtartiere@il4p.fr <rtartiere@il4p.fr@ba5c3050-9c55-11de-a262-85a698460a8e>
2010-07-07 17:34:12 +0000

    (libfreefare-desfire) Add missing svn:keywords properties for examples.

--- branches/libfreefare-desfire/examples/mifare-desfire-info.c
+++ branches/libfreefare-desfire/examples/mifare-desfire-info.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  * 
- * $Id: mifare-classic-format.c 189 2010-03-01 14:04:47Z romain.tartiere $
+ * $Id$
  */
 
 #include <err.h>
